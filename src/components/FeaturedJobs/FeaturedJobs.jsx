1b90db7f855d8cf6cd259cac0a9050c9a0de7cbd
sajib37 <sajibhasan9811@gmail.com>
2023-09-23 18:51:32 +0600

    show more jobs button handled completely

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -3,11 +3,23 @@ import FeaturedJob from "../FeaturedJob/FeaturedJob";
 
 const FeaturedJobs = () => {
     const [featuredJobs, setFeturedJobs] = useState([]);
+    const [dataLength, setDataLength] = useState(2);
     useEffect(() => {
-        fetch('./data/jobs.json')
-            .then(res => res.json())
-            .then(data => setFeturedJobs(data));
-    },[])
+        fetch("./data/jobs.json")
+            .then((res) => res.json())
+            .then((data) => setFeturedJobs(data));
+    }, []);
+
+    const handleShowMore = () => {
+        let dl = dataLength + 2;
+        if (dl < featuredJobs.length) {
+            setDataLength(dl);
+        }
+        else[
+            setDataLength(featuredJobs.length)
+        ]
+    }
+
     return (
         <div className="max-w-screen-xl mx-auto my-12">
             <div className="mb-8 text-center space-y-2">
@@ -18,9 +30,18 @@ const FeaturedJobs = () => {
                 </p>
             </div>
             <div className=" grid grid-cols-1 md:grid-cols-2 gap-12 px-2">
-                {
-                    featuredJobs.map((job,idx)=><FeaturedJob key={idx} job={job}></FeaturedJob>)
-                }
+                {featuredJobs.slice(0,dataLength).map((job, idx) => (
+                    <FeaturedJob key={idx} job={job}></FeaturedJob>
+                ))}
+            </div>
+
+            <div className="flex items-center justify-center">
+                <button
+                    className={`mt-4 bg-gradient-to-r from-[#8C9CFE] to-[#9774FF] w-36 p-3 rounded-xl text-white font-medium ${dataLength===featuredJobs.length?'hidden':''}`}
+                onClick={handleShowMore}>
+                    
+                    Show more jobs
+                </button>
             </div>
         </div>
     );
